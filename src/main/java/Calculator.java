2f4b15582efaa742d8ae2fc345e2dedf64cb12ce
Oumar DIALLO <umarjallow1@gmail.com>
2020-12-01 16:06:02 +0000

    Add methods from Class Calculator to the class Main

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,55 @@
+
 public class Calculator {
+
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+
+    public static int min(int a, int b) {
+        int res = a < b ? a : b;
+        return res;
+    }
+
+
+    public static int max(int a, int b) {
+        int res = (a > b) ? a : b;
+        return res;
+    }
+
+
+    public static int maxElement(int[] list) {
+        int maxValue = list[0];
+        for (int i = 1; i < list.length; i++) {
+            if (list[i] > maxValue) {
+                maxValue = list[i];
+            }
+        }
+        return maxValue;
+    }
+
+
+    public static int minElement(int[] list) {
+        int minValue = list[0];
+        for (int i = 1; i < list.length; i++) {
+            if (list[i] < minValue) {
+                minValue = list[i];
+            }
+        }
+        return minValue;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Main {
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // DECLARATION DE VARIABLES
+        int[] myNumbers = {4, 2, 3, 10, 9, 5, 13};
+
+        int a = 10;
+        int b = 5;
+
+
+        // START PROG
+
+        // Min ELEMENT
+        System.out.println("le plus petit nombre : " + Calculator.minElement(myNumbers));
+
+        // MAX ELEMENT
+        System.out.println("le plus grand nombre : " + Calculator.maxElement(myNumbers));
+
+        // SUM
+        System.out.println(a + " + " + b + " = " + Calculator.sum(a, b));
+
+        // MINUS
+        System.out.println(a + " - " + b + " = " + Calculator.minus(a, b));
+
+        // DIVIDE
+        System.out.println(a + " / " + b + " = " + Calculator.divide(a, b));
+
+        //MIN
+        System.out.println("la plus petite valeur entre  " + a + " et " + b + " : " + Calculator.min(a, b));
+
+        //MAX
+        System.out.println("la plus grande valeur entre  " + a + " et " + b + " : " + Calculator.max(a, b));
+
+        // END
+    }
 }
